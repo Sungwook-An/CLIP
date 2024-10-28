93f20a909320bce478a05b5db1f120002eaaebfd
mingming715 <richkang715@gmail.com>
2024-10-28 20:58:17 +0900

    241028 20:58 minseok commit

--- clip.py
+++ clip.py
@@ -29,6 +29,7 @@ from tqdm import tqdm
 
 # pip install transformers
 from transformers import AutoTokenizer, CLIPTextModel
+from imagenet_class import CLASSNAMES
 
 from efficientnet_pytorch import EfficientNet
 
@@ -130,12 +131,13 @@ class ImgProjectionHead(nn.Module):
         dropout_prob = args.drop_out
         self.fc = nn.Linear(input_dim, output_dim)
         self.dropout = nn.Dropout(p=dropout_prob)
-        self.logit_scale = nn.Parameter(torch.ones([]))
+        self.logit_scale = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
+        nn.init.constant_(self.logit_scale, np.log(1 / 0.07))
     
     def forward(self, x):
         x = self.dropout(x)
         x = self.fc(x)
-        return x, self.logit_scale
+        return x, self.logit_scale.exp()
     
 class TxtProjectionHead(nn.Module):
     def __init__(self, args, input_dim, output_dim):
@@ -191,15 +193,17 @@ def collate_fn(batch):
 # Contrastive loss function 정의
 def contrastive_loss(args, image_embeds, text_embeds, labels, logit_scale):
     # ########## Old loss
-    logits_per_image = logit_scale * torch.matmul(image_embeds, text_embeds.t())
-    logits_per_text = logit_scale * torch.matmul(text_embeds, image_embeds.t())
+    logits_per_image = logit_scale * image_embeds @ text_embeds.t()
+    logits_per_text = logits_per_image.t()
+    # logits_per_image = torch.matmul(logit_scale * image_embeds, text_embeds.t())
+    # logits_per_text = torch.matmul(text_embeds, (logit_scale * image_embeds).t())
     labels = torch.arange(len(image_embeds)).to(logits_per_image.device)
     # print(labels.size())
     labels = labels.cuda(args.gpu)
     
     image_loss = nn.CrossEntropyLoss()(logits_per_image, labels)
     text_loss = nn.CrossEntropyLoss()(logits_per_text, labels)
-    loss = (image_loss + text_loss)
+    loss = (image_loss + text_loss) / 2
     
     # ########## New loss
     # image_embeds = F.normalize(image_embeds, dim=1)
@@ -284,7 +288,7 @@ def ft_one_epoch(args, image_encoder, text_encoder, image_projection, text_proje
     # Loss 계산 및 back prop
     lp_ft_loss = contrastive_loss(args, image_embeds, text_embeds, labels, logit_scale)
 
-    similarities = torch.matmul(image_embeds, text_embeds.T)
+    similarities = logit_scale * torch.matmul(image_embeds, text_embeds.T)
 
     top1_pred_indices = similarities.argmax(dim=1)
     top1_pred_labels = labels[top1_pred_indices]
@@ -294,17 +298,15 @@ def ft_one_epoch(args, image_encoder, text_encoder, image_projection, text_proje
     total = labels.size(0)
     top1_accuracy = correct / total
     
-    strFormat = '%-6s%-5s%-20s%-10s%-11s%-8s\n'
-    strOut = strFormat % ('epoch=', epoch, 'lp_ft_top1_accuracy=', round(top1_accuracy*100, 4), 'lp_ft_loss=', round(lp_ft_loss.item(), 4))
+    # strFormat = '%-6s%-5s%-20s%-10s%-11s%-8s\n'
+    # strOut = strFormat % ('epoch=', epoch, 'lp_ft_top1_accuracy=', round(top1_accuracy*100, 4), 'lp_ft_loss=', round(lp_ft_loss.item(), 4))
     # print(strOut, end="")
     
     return top1_accuracy, lp_ft_loss
 
-
-
 def full_fine_tuning(args, image_encoder, text_encoder, image_projection, text_projection, train_loader, train_sampler, epoch, scheduler, optimizer):
-    image_encoder.train()
-    text_encoder.train()
+    image_encoder.eval()
+    text_encoder.eval()
     image_projection.train()
     text_projection.train()
     
@@ -349,6 +351,28 @@ def val_one_epoch(args, image_encoder, text_encoder, image_projection, text_proj
     labels_all.append(labels)
     
     return image_embeddings_all, text_embeddings_all, labels_all, logit_scale
+
+def get_metric(image_features, text_features, logit_scale):
+    metrics = {}
+    logits_per_image = (logit_scale * image_features @ text_features.t()).detach().cpu()
+    logits_per_text = logits_per_image.t().detach().cpu()
+
+    logits = {"image_to_text": logits_per_image, "text_to_image": logits_per_text}
+    ground_truth = torch.arange(len(text_features)).view(-1, 1)
+
+    for name, logit in logits.items():
+        ranking = torch.argsort(logit, descending=True)
+        preds = torch.where(ranking == ground_truth)[1]
+        preds = preds.detach().cpu().numpy()
+
+        metric = np.mean(preds < 1)
+        # metrics[f"{name}_mean_rank"] = preds.mean() + 1
+        # metrics[f"{name}_median_rank"] = np.floor(np.median(preds)) + 1
+        # for k in [1, 5, 10]:
+        #     metrics[f"{name}_R@{k}"] = np.mean(preds < k)
+
+    return metric
+
 def validation(args, image_encoder, text_encoder, image_projection, text_projection, val_loader, epoch, scheduler, optimizer):
     global BEST_ACC1
     
@@ -362,29 +386,71 @@ def validation(args, image_encoder, text_encoder, image_projection, text_project
     labels_all = []
     
     with torch.no_grad():
+        tokenizer_new = AutoTokenizer.from_pretrained(MODEL)
         if args.local_rank == 0:
             print('validation')
             for images, texts, labels in tqdm(val_loader):
-                image_embeddings_all, text_embeddings_all, labels_all, _ = val_one_epoch(args, image_encoder, text_encoder, image_projection, text_projection, image_embeddings_all, text_embeddings_all, labels_all, images, texts, labels)
+                image_embeddings_all, text_embeddings_all, labels_all, logit_scale = val_one_epoch(args, image_encoder, text_encoder, image_projection, text_projection, image_embeddings_all, text_embeddings_all, labels_all, images, texts, labels)
         else:
             for images, texts, labels in val_loader:
-                image_embeddings_all, text_embeddings_all, labels_all, _ = val_one_epoch(args, image_encoder, text_encoder, image_projection, text_projection, image_embeddings_all, text_embeddings_all, labels_all, images, texts, labels)
+                image_embeddings_all, text_embeddings_all, labels_all, logit_scale = val_one_epoch(args, image_encoder, text_encoder, image_projection, text_projection, image_embeddings_all, text_embeddings_all, labels_all, images, texts, labels)
     
         image_embeddings_all = torch.cat(image_embeddings_all)
         text_embeddings_all = torch.cat(text_embeddings_all)
         labels_all = torch.cat(labels_all)
-        
-        image_embeddings_all = F.normalize(image_embeddings_all, dim=1)
-        text_embeddings_all = F.normalize(text_embeddings_all, dim=1)
 
-        similarities = torch.matmul(image_embeddings_all, text_embeddings_all.t())
-        
-        similarities = similarities.argmax(dim=1)
-        
-        correct = (similarities == torch.arange(similarities.size(0)).cuda()).sum().item()
+        text_embeddings_new = []
 
-        total = text_embeddings_all.size(0)
+        for c in tqdm(CLASSNAMES):
+            texts = ["A photo of a {}.".format(c)]
+            texts = tokenizer_new(texts, return_tensors='pt', padding=True, truncation=True)
+            text_inputs = {k: v.squeeze(1).cuda(args.gpu) for k, v in texts.items()}
+            text_features = text_encoder(**text_inputs).pooler_output
+
+            # Feature embedding
+            text_embeds = text_projection(text_features)
+            text_embeddings_new.append(text_embeds)
+        
+        text_embeddings_new = torch.cat(text_embeddings_new)
+        print("text_embeddings_new :", text_embeddings_new.size())
+        print("image_embeddings_all :", image_embeddings_all.size())
+        print("labels_all :", labels_all.size())
+
+# #####EDit######
+        # # Normalize embeddings
+        # image_embeddings_all = F.normalize(image_embeddings_all, dim=1)
+        # text_embeddings_new = F.normalize(text_embeddings_new, dim=1)
+        
+        similarities = logit_scale * torch.matmul(image_embeddings_all, text_embeddings_new.T)
+        top1_pred_indices = ((similarities.argmax(dim=1))).to(torch.int)
+        
+        print(f"top1_pred_indices: {top1_pred_indices[:100]}")
+        print(f"labels_all: {labels_all[:100]}")
+        print(f"top1_pred_indices size: {top1_pred_indices.size()}, {top1_pred_indices.dtype}")
+        print(f"labels_all size: {labels_all.size()}, {labels_all.dtype}")
+        
+        # top1_pred_labels = labels_all[top1_pred_indices]
+
+        # Compute top-1 accuracy
+        correct = (top1_pred_indices == labels_all).sum().item()
+
+        total = labels_all.size(0)
         top1_accuracy = correct / total
+######EDit######
+        # image_embeddings_all = F.normalize(image_embeddings_all, dim=1)
+        # text_embeddings_all = F.normalize(text_embeddings_all, dim=1)
+
+        # similarities = torch.matmul(image_embeddings_all, text_embeddings_all.t())
+        # print(similarities.size())
+        # print("similarities :", similarities)
+        # similarities = similarities.argmax(dim=1)
+        
+        # correct = (similarities == torch.arange(similarities.size(0)).cuda()).sum().item()
+
+        # total = text_embeddings_all.size(0)
+        # top1_accuracy = correct / total
+
+        # top1_accuracy = get_metric(image_embeddings_all, text_embeddings_all, logit_scale)
         
         if args.local_rank == 0:
             print('top1_accuracy :', top1_accuracy*100, end=" ")
@@ -411,10 +477,10 @@ def validation(args, image_encoder, text_encoder, image_projection, text_project
     
     return top1_accuracy
 
-def save_checkpoint(state, is_best, filename='/root/LGD2024/examples_old/imagenet/clip_ckpt/last.pth.tar'):
+def save_checkpoint(state, is_best, filename='/home/mango/LGD-CLIP/clip_ckpt/last.pth.tar'):
     torch.save(state, filename)
     if is_best:
-        shutil.copyfile(filename, '/root/LGD2024/examples_old/imagenet/clip_ckpt/model_best.pth.tar')
+        shutil.copyfile(filename, '/home/mango/LGD-CLIP/clip_ckpt/model_best.pth.tar')
     
 # def zeroshot_classifier(args, image_encoder, text_encoder, image_projection, text_projection, val_loader):
 #     global MODEL
@@ -545,7 +611,7 @@ def main_worker(args):
         
     # 사전 학습된 모델 로드
     # Image encoder는 EfficientNet_-b1 사용, text encoder는 Transformer 사용
-    image_encoder = EfficientNet.from_pretrained(args.arch, weights_path='/root/LGD2024/examples_old/imagenet/model_best_blurpool_78_528.pth.tar', advprop=args.advprop)
+    image_encoder = EfficientNet.from_pretrained(args.arch, weights_path='/home/mango/LGD-CLIP/model_best_blurpool_78_528.pth.tar', advprop=args.advprop)
     
     if hasattr(image_encoder, '_fc'):
         image_encoder._fc = torch.nn.Identity()
@@ -591,15 +657,15 @@ def main_worker(args):
                 checkpoint = torch.load(args.resume, map_location=loc)
             args.start_epoch = checkpoint['epoch']
             best_acc1 = checkpoint['best_acc1']
-            if args.gpu is not None:
-                # best_acc1 may be from a checkpoint from a different GPU
-                best_acc1 = best_acc1.to(args.gpu)
+            # if args.gpu is not None:
+            #     # best_acc1 may be from a checkpoint from a different GPU
+            #     best_acc1 = best_acc1.to(args.gpu)
             image_encoder.load_state_dict(checkpoint['image_encoder_state_dict'])
             text_encoder.load_state_dict(checkpoint['text_encoder_state_dict'])
             image_projection.load_state_dict(checkpoint['image_projection_state_dict'])
             text_projection.load_state_dict(checkpoint['text_projection_state_dict'])
-            optimizer.load_state_dict(checkpoint['optimizer'])
-            scheduler.load_state_dict(checkpoint['scheduler'])
+            # optimizer.load_state_dict(checkpoint['optimizer'])
+            # scheduler.load_state_dict(checkpoint['scheduler'])
             print("=> loaded checkpoint '{}' (epoch {})"
                   .format(args.resume, checkpoint['epoch']))
         else:
@@ -621,8 +687,8 @@ def main_worker(args):
         normalize,
     ])
     
-    train_csv_file = '/root/LGD2024/examples_old/imagenet/imagenet_caption_train_with_labels.csv'
-    val_csv_file = '/root/LGD2024/examples_old/imagenet/imagenet_caption_val_with_labels.csv'
+    train_csv_file = '/home/mango/LGD-CLIP/imagenet_caption_train_with_labels.csv'
+    val_csv_file = '/home/mango/LGD-CLIP/imagenet_caption_val_with_labels.csv'
     train_df = pd.read_csv(train_csv_file)
     val_df = pd.read_csv(val_csv_file)
     
@@ -631,7 +697,7 @@ def main_worker(args):
     
     if args.distributed:
         train_sampler = distributed.DistributedSampler(dataset=train_dataset, shuffle=True)
-        val_sampler = distributed.DistributedSampler(dataset=val_dataset, shuffle=True)
+        val_sampler = distributed.DistributedSampler(dataset=val_dataset, shuffle=False)
     else:
         train_sampler = None
         val_sampler = None
@@ -671,15 +737,15 @@ def main_worker(args):
     args.ft_batch_size = args.batch_size
     
     optimizer = optim.Adam(list(image_projection.parameters()) + list(text_projection.parameters()), lr=args.lr, betas=(0.9, 0.98), eps=1e-6)
-    # scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, args.ft_epochs)
-    scheduler = WarmupCosineAnnealingLR(optimizer, warmup_epochs=args.warmup_epochs, max_epochs=args.ft_epochs+args.warmup_epochs, min_lr=args.lr/10)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, args.ft_epochs)
+    # scheduler = WarmupCosineAnnealingLR(optimizer, warmup_epochs=args.warmup_epochs, max_epochs=args.ft_epochs+args.warmup_epochs, min_lr=args.lr/10)
     
     if args.distributed:
         train_loader = DataLoader(train_dataset, batch_size=args.ft_batch_size//args.world_size, num_workers=args.workers, collate_fn=collate_fn, pin_memory=True, sampler=train_sampler)
         val_loader = DataLoader(val_dataset, batch_size=args.ft_batch_size//args.world_size, num_workers=args.workers, collate_fn=collate_fn, pin_memory=True, sampler=val_sampler)
     else:
         train_loader = DataLoader(train_dataset, batch_size=args.ft_batch_size, shuffle=True, num_workers=args.workers, collate_fn=collate_fn, pin_memory=True)
-        val_loader = DataLoader(val_dataset, batch_size=args.ft_batch_size, shuffle=True, num_workers=args.workers, collate_fn=collate_fn, pin_memory=True)
+        val_loader = DataLoader(val_dataset, batch_size=args.ft_batch_size, shuffle=False, num_workers=args.workers, collate_fn=collate_fn, pin_memory=True)
     
     prev_top1_accuracy = 0
     count = 0
@@ -687,22 +753,15 @@ def main_worker(args):
     image_projection._set_static_graph()
     
     for param in image_encoder.parameters():
-        param.requires_grad = True
+        param.requires_grad = False
     for param in text_encoder.parameters():
-        param.requires_grad = True
+        param.requires_grad = False
     for epoch in range(args.ft_epochs):
         if args.distributed:
             train_sampler.set_epoch(epoch)
         full_fine_tuning(args, image_encoder, text_encoder, image_projection, text_projection, train_loader, train_sampler, epoch, scheduler, optimizer)
-        top1_accuracy = validation(args, image_encoder, text_encoder, image_projection, text_projection, val_loader, epoch, scheduler, optimizer)
-        # if prev_top1_accuracy < top1_accuracy:
-        #     prev_top1_accuracy = top1_accuracy
-        #     count = 0
-        # else:
-        #     count += 1
-        #     if count > 20:
-        #         break
-
+        top1_acc = validation(args, image_encoder, text_encoder, image_projection, text_projection, val_loader, epoch, scheduler, optimizer)
+        print(f"top1_acc: {top1_acc}")
 
 if __name__ == '__main__':
     main()
--- clip_ddp.sh
+++ clip_ddp.sh
@@ -1,8 +1,7 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun \
-    --nproc_per_node=4 \
+CUDA_VISIBLE_DEVICES=2,3 torchrun \
+    --nproc_per_node=2 \
     --nnodes=1 \
     --node_rank=0 \
     --master_addr="localhost" \
     --master_port=1234 \
-    clip.py \
-    --batch_size 256 \
+    clip.py --batch_size 1024 --lr 1e-4
