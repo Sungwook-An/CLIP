893064aa4365e1eab61eff0838aff188c564880e
Sungwook-An <asu970602@gmail.com>
2024-11-05 16:53:59 +0900

    241105 16:52 asu commit

--- clip_ddp.sh
+++ clip_ddp.sh
@@ -5,7 +5,7 @@
 #     --node_rank=0 \
 #     --master_addr="localhost" \
 #     --master_port=1234 \
-#     clip.py --base_path='/home/mango/LGD-CLIP' --batch_size 512 --lr 1e-5 --train_type teft --epochs 30
+#     lgd_clip.py --base_path='/home/mango/LGD-CLIP' --batch_size 512 --lr 1e-5 --train_type teft --epochs 30
     
 # # 민석 validate
 # CUDA_VISIBLE_DEVICES=2,3 torchrun \
@@ -14,7 +14,7 @@
 #     --node_rank=0 \
 #     --master_addr="localhost" \
 #     --master_port=1234 \
-#     clip.py --base_path='/home/mango/LGD-CLIP' --batch_size 512 --validate --train_type teft --resume "/home/mango/LGD-CLIP/clip_ckpt/model_best.pth.tar"
+#     lgd_clip.py --base_path='/home/mango/LGD-CLIP' --batch_size 512 --validate --train_type teft --resume "/home/mango/LGD-CLIP/clip_ckpt/model_best.pth.tar"
     
 # 민석 classification
 # CUDA_VISIBLE_DEVICES=2,3 torchrun \
@@ -23,7 +23,7 @@
 #     --node_rank=0 \
 #     --master_addr="localhost" \
 #     --master_port=1234 \
-#     clip.py --base_path='/home/mango/LGD-CLIP' --batch_size 1024 --classification --train_type lp --resume "/home/mango/LGD-CLIP/clip_ckpt_LP/last.pth.tar"
+#     lgd_clip.py --base_path='/home/mango/LGD-CLIP' --batch_size 1024 --classification --train_type lp --resume "/home/mango/LGD-CLIP/clip_ckpt_LP/last.pth.tar"
 
 # 성욱
 CUDA_VISIBLE_DEVICES=2,3 torchrun \
@@ -32,4 +32,4 @@ CUDA_VISIBLE_DEVICES=2,3 torchrun \
     --node_rank=0 \
     --master_addr="localhost" \
     --master_port=1234 \
-    clip.py --base_path='/root/LGD2024/examples_old/imagenet' --batch_size 512 --lr 1e-4 --eta_min 1e-6 --train_type teft --warmup_epochs 10
+    lgd_clip.py --base_path='/root/LGD2024/examples_old/imagenet' --batch_size 512 --lr 1e-4 --eta_min 1e-6 --train_type teft --warmup_epochs 10
